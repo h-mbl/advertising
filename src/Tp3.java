6ba017f176aa4e53bc13eaa1776cb3bde950cd20
herve.ngisse <hervembl3@gmail.com>
2023-07-27 18:16:42 -0600

    init

---
+++ src/Tp3.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tp3 {
+    static List<String> sommet = new ArrayList<>();
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Fournissez les noms des fichiers d'entrée et de sortie.");
+            return;
+        }
+        String inputFileCarte= "src/testsTP3E19/" + args[0];
+        String outputFileArm = "/testsTP3E19/" + args[1];
+        try (FileReader fileReader = new FileReader(inputFileCarte);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            String line;
+            while ((line = bufferedReader .readLine()) != null) {
+                if (line.length() == 2){
+                   sommet.add(line);
+                }
+                else if(Character.isLetter(line.charAt(0))) {
+                    line = line.replace(";", "");
+                    line = line.replace(":", "");
+                    line = line.trim().replaceAll("\\s+", " ");
+                    String[] newLine = line.split(" ");
+                    String nomArete = newLine[0];
+                    String sommetDepart = newLine[1];
+                    String sommetArrivee = newLine[2];
+                    int poidArete = Integer.parseInt(newLine[3]);
+
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
